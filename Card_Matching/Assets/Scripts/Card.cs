4a348bb2c3707a1d160065f83c4e4166e99e33fb
Nilesh Sharma <nilesh.12th@gmail.com>
2025-08-10 16:19:58 +0530

    working prototype ready

--- Card_Matching/Assets/Scripts/Card.cs
+++ Card_Matching/Assets/Scripts/Card.cs
@@ -2,35 +2,55 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
+
 
 public class Card : MonoBehaviour
 {
-
-    [SerializeField] private Image iconImage;
+    public Image iconImage;
 
     public Sprite hiddenIconSprite;
     public Sprite iconSprite;
 
     public bool isSelected;
 
-
+    public CardsController cardCon;
     public void SetIconSprite(Sprite sprite)
     {
         iconSprite = sprite;
     }
 
-
     public void Show()
     {
-        iconImage.sprite = iconSprite;
-        isSelected = true;
+        //iconImage.sprite = null;
+        transform.DORotate(new Vector3(0f, 90f, 0f), 0.1f, RotateMode.Fast).OnComplete(() =>
+        {
+            iconImage.sprite = iconSprite;
+            transform.DORotate(new Vector3(0f, 180f, 0f), 0.1f, RotateMode.Fast).OnComplete(() =>
+            {
+                isSelected = true;
+            });
+        });
+        //iconImage.sprite = iconSprite;
+        //DOVirtual.DelayedCall(0.1f, () => iconImage.sprite = iconSprite);
     }
 
     public void Hide()
     {
-        iconImage.sprite = hiddenIconSprite;
-        isSelected = false;
+        transform.DORotate(new Vector3(0f, 90f, 0f), 0.1f, RotateMode.Fast).OnComplete(() =>
+        {
+            iconImage.sprite = hiddenIconSprite;
+            transform.DORotate(new Vector3(0f, 0f, 0f), 0.1f, RotateMode.Fast).OnComplete(() =>
+            {
+                isSelected = false;
+            });
+        });
 
+    }
+
+    public void OnClick()
+    {
+        cardCon.SetSelected(this);
     }
 
     // Start is called before the first frame update
--- Card_Matching/Assets/Scripts/CardsController.cs
+++ Card_Matching/Assets/Scripts/CardsController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 public class CardsController : MonoBehaviour
 {
@@ -12,14 +13,18 @@ public class CardsController : MonoBehaviour
 
     private List<Sprite> spritePairs;
 
+    Card firstSelected, secSelected;
 
+    readonly List<Card> _openCards = new List<Card>();                
+    readonly Queue<(Card a, Card b)> _compareQueue = new Queue<(Card, Card)>();
+    bool _isProcessingQueue;
     private void Start()
     {
         SetupSprite();
         CreateCards();
     }
 
-    private void SetupSprite()
+    private void SetupSprite()  // no of sprites decided here!
     {
         spritePairs = new List<Sprite>();
         for(int i = 0; i < sprites.Length; i++)
@@ -37,6 +42,7 @@ public class CardsController : MonoBehaviour
         {
             Card card = Instantiate(cardPrefab, gridTransform);
             card.SetIconSprite(spritePairs[i]);
+            card.cardCon = this;
         }
     }
 
@@ -50,4 +56,67 @@ public class CardsController : MonoBehaviour
             spritesList[randomIndex] = temp;
         }
     }
+
+    public void SetSelected(Card card)
+    {
+        if (!card.isSelected)
+        {
+            card.Show();
+
+            _openCards.Add(card);
+
+            while (_openCards.Count >= 2)
+            {
+                var a = _openCards[0];
+                var b = _openCards[1];
+                _openCards.RemoveAt(0);
+                _openCards.RemoveAt(0);
+                _compareQueue.Enqueue((a, b));
+            }
+
+            if (!_isProcessingQueue)
+                StartCoroutine(ProcessCompareQueue());
+        }
+    }
+    IEnumerator ProcessCompareQueue()
+    {
+        _isProcessingQueue = true;
+
+        while (_compareQueue.Count > 0)
+        {
+            var (a, b) = _compareQueue.Dequeue();
+
+            yield return new WaitForSeconds(0.25f);
+
+            if (a == null || b == null) continue;
+
+            if (a.iconSprite == b.iconSprite)
+            {
+                a.transform.DOPunchScale(Vector3.one * 0.1f, 0.15f, 8, 0.9f);
+                b.transform.DOPunchScale(Vector3.one * 0.1f, 0.15f, 8, 0.9f);
+
+                var tempCol = Color.white;
+                tempCol.a = 0.65f;
+                a.iconImage.color = tempCol;
+                b.iconImage.color = tempCol;
+            }
+            else
+            {
+                StartCoroutine(FlipBackAfterDelay(a, b, 0.35f));
+            }
+
+            yield return null;
+        }
+
+        _isProcessingQueue = false;
+    }
+
+    IEnumerator FlipBackAfterDelay(Card a, Card b, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+
+        if (a != null && a.isSelected) a.Hide();
+        if (b != null && b.isSelected) b.Hide();
+    }
+
 }
